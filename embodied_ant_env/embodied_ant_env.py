5f44bdf5a67f7c9256c964162cb4c056740f4124
Patrick Spieler <stapelzeiger@gmail.com>
2025-07-16 23:06:44 -0600

    rewrite IMU driver, threading & step rate control

--- embodied_ant_env/embodied_ant_env.py
+++ embodied_ant_env/embodied_ant_env.py
@@ -1,10 +1,9 @@
 import numpy as np
+from imu_msp import IMU_MSP
+import threading
+import time
 
 class MotorController:
-    def __init__(self, port):
-        pass
-
-class IMU:
     def __init__(self, port):
         pass
 
@@ -18,24 +17,76 @@ class Space:
 class EmbodiedAnt:
     action_space = Space(shape=(8,), dtype=np.float32)
     observation_space = Space(shape=(10,), dtype=np.float32)
-    
-    def __init__(self, motor_port, imu_port):
+
+    def __init__(self, motor_port, imu_port, step_size=0.02):
         self.motor_port = motor_port
         self.imu_port = imu_port
         self.motor_controller = MotorController(motor_port)
-        self.imu = IMU(imu_port)
+        self.step_size = step_size
+        self.last_step_time = None
+
+        self._threads_should_exit = False
+
+        self.imu = IMU_MSP(imu_port)
+        self._imu_data = None
+        self._imu_data_lock = threading.Lock()
+        self._imu_thread = threading.Thread(target=self._poll_imu, daemon=True)
+        self._imu_thread.start()
+
+    def __del__(self):
+        self.close()
 
     def reset(self):
         pass
 
-    def step(self, action):
-        pass
+    def step(self, action, sleep_until_next_step=True, sync_observation_with_action=False):
+        if sync_observation_with_action:
+            observation = self.get_observation()
+
+        # send actuators
+        # self.motor_controller.send_actuators(action)
+
+        sleep_duration = self.step_size
+        if self.last_step_time is not None:
+            time_since_last_step = time.time() - self.last_step_time
+            sleep_duration = self.step_size - time_since_last_step
+            if sleep_duration < 0:
+                print(f"Warning: calls to step() exceeded step size (time since last step: {time_since_last_step:.3f}s).")
+                sleep_duration = 0
+        if sleep_until_next_step:
+            time.sleep(sleep_duration)
+
+        if not sync_observation_with_action:
+            observation = self.get_observation()
+
+        self.last_step_time = time.time()
+        return observation
+
+    def get_observation(self):
+        with self._imu_data_lock:
+            imu_data = self._imu_data
+        return imu_data
 
     def render(self, mode='human'):
         if mode == 'human':
-            pass
+            print("render(mode='human') this is the real world, look at your robot!")
         elif mode == 'rgb_array':
             return self.camera.get_image()
 
     def close(self):
-        pass
+        self._threads_should_exit = True
+        self._imu_thread.join()
+
+    def _poll_imu(self):
+        while not self._threads_should_exit:
+            imu_data = self.imu.get_data()
+            with self._imu_data_lock:
+                self._imu_data = imu_data
+
+
+if __name__ == "__main__":
+    import sys
+    env = EmbodiedAnt(motor_port=sys.argv[1], imu_port=sys.argv[2])
+    while True:
+        # time.sleep(0.03)
+        print(env.step(np.zeros(8)))
--- embodied_ant_env/imu_msp.py
+++ embodied_ant_env/imu_msp.py
@@ -2,75 +2,103 @@ import serial
 import sys
 import struct
 import time
-
-# based on https://github.com/tonbut/rpi-python-drone/blob/master/air/pyMultiWii/pyMultiwii.py
-# TODO for scaling check this:
-# https://github.com/betaflight/betaflight-configurator/blob/aeda56ba407ba54068bad90d7cc069b67d2cd8e4/src/js/msp/MSPHelper.js#L116-L131
+import math
 
 class IMU_MSP:
     RAW_IMU = 102
     ATTITUDE = 108
 
+    """
+    MSP Packet Format (MSPv1)
+
+    Request (host → flight controller):
+        '$' 'M' '<' [payload_size:1B] [command:1B] [payload:N bytes] [checksum:1B]
+
+    Response (flight controller → host):
+        '$' 'M' '>' [payload_size:1B] [command:1B] [payload:N bytes] [checksum:1B]
+
+    - Header:      3 bytes ('$', 'M', '<' or '>')
+    - Payload Size: number of bytes in payload (0–255)
+    - Command:     8-bit command ID (e.g., 108 = MSP_ATTITUDE)
+    - Payload:     binary data specific to the command
+    - Checksum:    XOR of [payload_size, command, payload...]
+
+    All integers are little-endian. Payload interpretation depends on the command.
+    """
+
+
     def __init__(self, port: str, baudrate: int = 115200):
-        self.ser = serial.Serial(port, baudrate)
+        self.device = serial.Serial(port, baudrate)
 
-    def sendCMD(self, data_length, code, data):
+    @staticmethod
+    def checksum(data):
         checksum = 0
-        total_data = [b'$', b'M', b'<', data_length, code] + data
-        for i in struct.pack('<2B%dH' % len(data), *total_data[3:len(total_data)]):
+        for i in data:
             checksum = checksum ^ i
-        total_data.append(checksum)
-        try:
-            b = None
-            b = self.ser.write(struct.pack(f'<3c2B{len(data)}HB', *total_data))
-        except Exception as error:
-            print("\n\nError in sendCMD.")
-            print("("+str(error)+")\n\n")
-            pass
-        return b
+        return checksum
 
+    def send_cmd(self, cmd, data=[]):
+        cmd_bytes = struct.pack('<B', cmd)
+        data_bytes = struct.pack(f'<{len(data)}H', *data)
+        data_length_code = struct.pack('<B', len(data_bytes))
+        header = b'$M<'
+        checksum = self.checksum(data_length_code + cmd_bytes + data_bytes)
+        msg = header + data_length_code + cmd_bytes + data_bytes + struct.pack('<B', checksum)
+        self.device.write(msg)
 
-    def getData(self, cmd):
-        start = time.time()
-        # self.ser.flushInput()
-        # self.ser.flushOutput()
-        self.sendCMD(0,cmd,[])
+    def read_cmd(self, cmd):
+        start_time = time.time()
+        self.send_cmd(cmd,[])
         while True:
-            header = self.ser.read()
+            header = self.device.read(1)
             if header == b'$':
-                header = header+self.ser.read(2)
+                header = header+self.device.read(2)
                 break
-        datalength = struct.unpack('<b', self.ser.read())[0]
-        # print(datalength)
-        code_ = struct.unpack('<b', self.ser.read())
-        data = self.ser.read(datalength)
-        temp = struct.unpack(f'<{datalength//2}h',data)
-        self.ser.flushInput()
-        self.ser.flushOutput()
-        elapsed = time.time() - start
-        # print(temp)
+        if header != b'$M>':
+            print(f"unexpected header: {header} != $M<")
+            return None
+        header_args = self.device.read(2)
+        data_length, code = struct.unpack('<BB', header_args)
+        if code != cmd:
+            print(f"received code: {code} != cmd: {cmd}")
+            return None
+        data_bytes = self.device.read(data_length)
+        data = struct.unpack(f'<{data_length//2}h',data_bytes)
+        checksum = struct.unpack('<B', self.device.read(1))[0]
+        if checksum != self.checksum(header_args + data_bytes):
+            print(f"checksum: {checksum} != {self.checksum(data_bytes)}")
+            return None
+        self.device.flushInput()
+        self.device.flushOutput()
         if cmd == self.ATTITUDE:
             attitude = {}
-            attitude['angx']=float(temp[0]/10.0)
-            attitude['angy']=float(temp[1]/10.0)
-            attitude['heading']=float(temp[2])
-            attitude['elapsed']=round(elapsed,3)
-            attitude['timestamp']="%0.2f" % (time.time(),) 
+            attitude['roll_deg']=data[0] * 0.1
+            attitude['pitch_deg']=data[1] * 0.1
+            attitude['yaw_deg']=data[2] * 0.1
+            attitude['timestamp']=start_time
             return attitude
-        if cmd == self.RAW_IMU:
+        elif cmd == self.RAW_IMU:
             rawIMU = {}
-            rawIMU['ax']=float(temp[0])
-            rawIMU['ay']=float(temp[1])
-            rawIMU['az']=float(temp[2])
-            rawIMU['gx']=float(temp[3])
-            rawIMU['gy']=float(temp[4])
-            rawIMU['gz']=float(temp[5])
-            rawIMU['mx']=float(temp[6])
-            rawIMU['my']=float(temp[7])
-            rawIMU['mz']=float(temp[8])
-            rawIMU['elapsed']=round(elapsed,5)
-            rawIMU['timestamp']="%0.3f" % (time.time(),)
+            # https://github.com/betaflight/betaflight-configurator/blob/aeda56ba407ba54068bad90d7cc069b67d2cd8e4/src/js/msp/MSPHelper.js#L116-L131
+            rawIMU['ax']=data[0] / 512.0 * 9.81
+            rawIMU['ay']=data[1] / 512.0 * 9.81
+            rawIMU['az']=data[2] / 512.0 * 9.81
+            rawIMU['wx']=data[3] * (4 / 16.4) * math.pi / 180.0
+            rawIMU['wy']=data[4] * (4 / 16.4) * math.pi / 180.0
+            rawIMU['wz']=data[5] * (4 / 16.4) * math.pi / 180.0
+            rawIMU['mx']=data[6] / 1090.0
+            rawIMU['my']=data[7] / 1090.0
+            rawIMU['mz']=data[8] / 1090.0
+            rawIMU['timestamp']=start_time
             return rawIMU
+        else:
+            print(f"unknown command: {cmd}")
+            return None
+
+    def get_data(self):
+        attitude = self.read_cmd(self.ATTITUDE)
+        raw_imu = self.read_cmd(self.RAW_IMU)
+        return {**attitude, **raw_imu}
 
 
 if __name__ == "__main__":
@@ -78,8 +106,8 @@ if __name__ == "__main__":
     prev_time = time.time()
     delta_avg = 0
     while True:
-        print(imu.getData(imu.RAW_IMU))
-        print(imu.getData(imu.ATTITUDE))
+        print(imu.read_cmd(imu.ATTITUDE))
+        print(imu.read_cmd(imu.RAW_IMU))
         now = time.time()
         delta = now - prev_time
         prev_time = now
