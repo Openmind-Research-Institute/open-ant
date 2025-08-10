1720c612cdab33a26e358c03021025a7b2369a34
Patrick Spieler <stapelzeiger@gmail.com>
2025-08-10 11:12:21 -0600

    stateful options

--- agents/dqn_with_options.py
+++ agents/dqn_with_options.py
@@ -12,13 +12,13 @@ import sys
 
 hw_config = sys.argv[1] if len(sys.argv) > 1 else None
 render = "human"
-DT = 0.02
+DT = 0.05
 
 joint_config = {
             'hip_zero': 0,
             'knee_zero': -np.radians(60),
             'hip_range': np.radians(45),
-            'knee_range': np.radians(60),
+            'knee_range': np.radians(30),
         }
 
 if hw_config is None:
@@ -40,101 +40,88 @@ else:
                        joint_config=joint_config)
 
 
-def option(start_pos: float, end_pos: float, duration: float):
+
+def ramp(start_pos: float, end_pos: float, duration: float):
     input_pos_list = [start_pos]
     for i in range(int(duration / DT)):
         input_pos_list.append(input_pos_list[-1] + (end_pos - start_pos) / (duration / DT))
     return input_pos_list
 
 env.reset()
-o_move_hip_positive_dir = option(start_pos=-joint_config['hip_range'], end_pos=joint_config['hip_range'], duration=1)
-o_move_hip_negative_dir = option(start_pos=joint_config['hip_range'], end_pos=-joint_config['hip_range'], duration=1)
-o_move_knee_positive_dir = option(start_pos=-joint_config['knee_range'], end_pos=joint_config['knee_range'], duration=1)
-o_move_knee_negative_dir = option(start_pos=joint_config['knee_range'], end_pos=-joint_config['knee_range'], duration=1)
 
-time_list = np.arange(len(o_move_hip_positive_dir)) * DT
-plt.plot(time_list, o_move_hip_positive_dir, label='Option 1: hip positive')
-plt.plot(time_list, o_move_hip_negative_dir, label='Option 2: hip negative')
-plt.plot(time_list, o_move_knee_positive_dir, label='Option 3: knee positive')
-plt.plot(time_list, o_move_knee_negative_dir, label='Option 4: knee negative')
-# plt.legend(loc='upper right')
-plt.xlabel('Time (s)')
-plt.ylabel('Position (rad)')
-plt.title('Options')
 
-# List of all options
-options_dict = {
-    'o_move_hip_positive_dir': {
-        'option': o_move_hip_positive_dir,
-        'joint_names': ['hip'], 
-    },
-    'o_move_hip_negative_dir': {
-        'option': o_move_hip_negative_dir,
-        'joint_names': ['hip'],
-    },
-    'o_move_knee_positive_dir': {
-        'option': o_move_knee_positive_dir,
-        'joint_names': ['ankle'],
-    },
-    'o_move_knee_negative_dir': {
-        'option': o_move_knee_negative_dir,
-        'joint_names': ['ankle'],
-    }
-}
+options = []
+for i in range(4):
+    # hip
+    options.append({
+        "joint": 2*i,
+        "target": np.radians(30),
+        "duration": 0.3
+    })
+    options.append({
+        "joint": 2*i,
+        "target": -np.radians(30),
+        "duration": 0.3
+    })
+    # knee
+    options.append({
+        "joint": 2*i + 1,
+        "target": np.radians(20),
+        "duration": 0.3
+    })
+    options.append({
+        "joint": 2*i + 1,
+        "target": -np.radians(20),
+        "duration": 0.3
+    })
 
-expanded_options_dict = {}
-for base_name, data in options_dict.items():
-    for i in range(1, 5):
-        new_name = f"{base_name}_{i}"
 
-        # Update joint_names: hip -> hip_i, ankle -> ankle_i.
-        updated_joint_names = [f"{name}_{i}" for name in data['joint_names']]
+np.set_printoptions(precision=4, suppress=True, linewidth=120, threshold=1000)
 
-        expanded_options_dict[new_name] = {
-            'option': data['option'],
-            'joint_names': updated_joint_names
-        }
+class OptionEnv:
+    def __init__(self, env, options, discount=0.99):
+        self.env = env
+        self.options = options
+        self.discount = discount
+        self.joint_pos = np.zeros(len(env.q_joints))
+    
+    def step(self, option: int):
+        opt = self.options[option]
+        traj = ramp(self.joint_pos[opt['joint']], opt['target'], opt['duration'])
+        print("from to", self.joint_pos[opt['joint']], opt['target'])
+        total_reward = 0
+        for i in range(int(opt['duration'] / DT)):
+            self.joint_pos[opt['joint']] = traj[i]
+            print( traj[i])
+            print(self.joint_pos)
+            obs, reward, terminated, truncated, info = self.env.step(self.joint_pos)
+            print(self.joint_pos)
+            total_reward = reward + self.discount * total_reward
+            if terminated or truncated:
+                return obs, total_reward, terminated, truncated, info
+        print("final", self.joint_pos)
+        return obs, total_reward, terminated, truncated, info
+
+    def reset(self):
+        self.joint_pos = np.zeros(len(self.env.q_joints))
+        return self.env.reset()
+    
+    def render(self):
+        return self.env.render()
+
+
+options_env = OptionEnv(env, options)
+
+while True:
+    obs, reward, terminated, truncated, info = options_env.step(np.random.randint(len(options)))
+    if hw_config is None:
+        options_env.render()
+    # if terminated or truncated:
+    #     options_env.reset()
 
 
 import torch.nn as nn
 import torch.nn.functional as F
-
-import random
-options_list = list(expanded_options_dict.keys())
-N = 10
-time_ = 0
-action_list = []
-time_list = []
-for i in range(N):
-    # Pick a random option.
-    current_option = random.choice(options_list)
-    print('Current option: ', current_option)
-    # Execute the current option fully.
-    for j in range(len(expanded_options_dict[current_option]['option'])):
-        o_value = expanded_options_dict[current_option]['option'][j]
-        action = np.zeros(len(env.q_joints))
-        for joint_name in expanded_options_dict[current_option]['joint_names']:
-            action[env.q_joints[joint_name] - 1] = o_value
-        obs, reward, terminated, truncated, info = env.step(action)
-        if hw_config is None:
-            env.render()
-        action_list.append(action)
-        time_ += DT
-        time_list.append(time_)
-
-action_np = np.array(action_list)
-fig, ax = plt.subplots(len(env.name_joints)//2, 2)
-for i in range(len(env.name_joints)//2):
-    ax[i, 0].plot(time_list, action_np[:, i])
-    ax[i, 0].set_title(env.name_joints[i])
-    ax[i, 0].set_xlabel('Time (s)')
-    ax[i, 0].set_ylabel('Action')
-    ax[i, 1].plot(time_list, action_np[:, i + len(env.name_joints)//2])
-    ax[i, 1].set_title(env.name_joints[i + len(env.name_joints)//2])
-    ax[i, 1].set_xlabel('Time (s)')
-    ax[i, 1].set_ylabel('Action')
-plt.tight_layout()
-plt.show()
 
 
 class QFunction(nn.Module):
