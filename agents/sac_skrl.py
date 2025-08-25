dedf0fe606336a41bb3234e553ebf21f8e1f9faa
Sorina <lupusorina@yahoo.com>
2025-08-24 17:17:19 -0700

    tuning, split into train and test

--- agents/sac_skrl.py
+++ agents/sac_skrl.py
@@ -2,13 +2,14 @@ from skrl.agents.torch.sac import SAC, SAC_DEFAULT_CONFIG
 from skrl.memories.torch import RandomMemory
 from skrl.models.torch import DeterministicMixin, GaussianMixin, Model
 from skrl.envs.wrappers.torch import wrap_env
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+from skrl.resources.preprocessors.torch import RunningStandardScaler
 
 import sys
 import os
 import json
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../sim')))
 from ant_mujoco import AntEnv
@@ -56,7 +57,7 @@ def run(agent, env):
     i = 0
     time_window = 10.0  # seconds
     window_size = int(time_window / env.dt)
-    moving_average_reward_queue = deque(maxlen=window_size)
+    moving_reward_queue = deque(maxlen=window_size)
 
     df = pd.DataFrame(columns=['step', 'reward'])
     while True:
@@ -66,7 +67,8 @@ def run(agent, env):
             next_obs, reward, terminated, truncated, info = env.step(action)
             # env.render()
             agent.record_transition(obs, action, reward, next_obs, terminated, truncated, info, i, -1)
-        agent.post_interaction(i, -1)
+        if TRAIN == True:
+            agent.post_interaction(i, -1)
         obs = next_obs
         i += 1
 
@@ -74,14 +76,14 @@ def run(agent, env):
             obs, info = env.reset()
 
         reward_per_second = reward.item()/env.dt
-        moving_average_reward_queue.append(reward_per_second)
-        if len(moving_average_reward_queue) > window_size:
+        moving_reward_queue.append(reward_per_second)
+        if len(moving_reward_queue) > window_size:
             # Pop the leftmost element.
-            moving_average_reward_queue.popleft()
+            moving_reward_queue.popleft()
 
-        average_reward_per_second = sum(moving_average_reward_queue) / len(moving_average_reward_queue)
+        average_reward_per_second = sum(moving_reward_queue) / len(moving_reward_queue)
         if i % 5000 == 0:
-            print(f"Step {i}, moving average reward {average_reward_per_second:.4f}")
+            print(f"Step {i}, time [s] {i * env.dt:.2f}, time [min] {i * env.dt / 60:.2f}, moving average reward {average_reward_per_second:.4f}")
             df = pd.concat([df, pd.DataFrame({'step': [i], 'reward': [average_reward_per_second]})], ignore_index=True)
             df.to_csv(os.path.join(LOG_FOLDER, f'rewards_{DATE_NOW}.csv'), index=False)
 
@@ -90,15 +92,18 @@ LOG_FOLDER = 'logs_sac_skrl'
 if not os.path.exists(LOG_FOLDER):
     os.makedirs(LOG_FOLDER)
 
+TRAIN = True
+
+DT = 0.05
 if len(sys.argv) > 1:
     config_file = sys.argv[1]
     with open(config_file, 'r') as f:
         cfg = json.load(f)
-    env = make_ant_env(cfg, render_mode='human', dt=0.05)
+    env = make_ant_env(cfg, render_mode='human', dt=DT)
 else:
     env = AntEnv(
         # render_mode="human",
-                 dt=0.05)
+                 dt=DT)
 
 env = wrap_env(env, "gymnasium")
 device = env.device
@@ -113,15 +118,27 @@ for model in models.values():
     model.init_parameters(method_name="normal_", mean=0.0, std=0.1)
 
 cfg = SAC_DEFAULT_CONFIG.copy()
-cfg["discount_factor"] = 0.98
-cfg["batch_size"] = 100
-cfg["random_timesteps"] = 0
-cfg["learning_starts"] = 1000
+cfg["gradient_steps"] = 1
+cfg["batch_size"] = 4096
+cfg["discount_factor"] = 0.99
+cfg["polyak"] = 0.005
+cfg["actor_learning_rate"] = 5e-4
+cfg["critic_learning_rate"] = 5e-4
+cfg["random_timesteps"] = 80
+cfg["learning_starts"] = 80
+cfg["grad_norm_clip"] = 0
 cfg["learn_entropy"] = True
+cfg["entropy_learning_rate"] = 5e-3
+cfg["initial_entropy_value"] = 1.0
+cfg["state_preprocessor"] = RunningStandardScaler
+cfg["state_preprocessor_kwargs"] = {"size": env.observation_space, "device": device}
 # logging to TensorBoard and write checkpoints (in timesteps)
-cfg["experiment"]["write_interval"] = 10
-cfg["experiment"]["checkpoint_interval"] = 1000
-cfg["experiment"]["directory"] = "runs/sac/"
+
+# Configure and instantiate the RL trainer. 
+time_in_hours = 10 # 10 hours
+total_timesteps = int(time_in_hours * 3600 / DT)
+# Record every 30 minutes.
+cfg["experiment"]["checkpoint_interval"] = int(30 * 60 / DT)
 
 memory = RandomMemory(memory_size=20000, device=device, replacement=False)
 
@@ -134,4 +151,9 @@ agent = SAC(models=models,
 
 agent.init()
 
-run(agent, env)
+if TRAIN == False:
+    agent.load('/home/sorina/embodied-mujoco-ant/agents/logs_ppo_skrl_v2/25-08-23_19-01-31-418897_PPO/checkpoints/agent_85000.pt')
+    agent.set_mode("eval")
+    run(agent, env)
+else:
+    run(agent, env)
