ce48c6973621e52c93ed104aaa1776dd968087b6
Sorina Lupu <lupusorina@yahoo.com>
2025-08-25 17:02:11 -0700

    cleanup reward function

--- agents/reward.py
+++ agents/reward.py
@@ -3,13 +3,14 @@ import pandas as pd
 from collections import deque
 
 class RewardTracker:
-    def __init__(self, env_dt, time_window=10.0, log_folder=".", date_now="log"):
+    def __init__(self, env_dt, env_id, time_window=10.0, log_folder=".", date_now="log"):
         self.env_dt = env_dt
         self.window_size = int(time_window / env_dt)
         self.queue = deque(maxlen=self.window_size)
         self.df = pd.DataFrame(columns=["step", "reward"])
         self.log_folder = log_folder
         self.date_now = date_now
+        self.env_id = env_id
 
     def update(self, reward):
         reward_per_second = reward / self.env_dt
@@ -19,12 +20,11 @@ class RewardTracker:
         return average_reward_per_second
 
     def log(self, step, average_reward_per_second, log_interval=5000):
-        if step % log_interval == 0 and step > 0:
-            print(f"Step {step}, time [s] {step * self.env_dt:.2f}, "
-                  f"time [min] {step * self.env_dt / 60:.2f}, "
-                  f"moving average reward {average_reward_per_second:.4f}")
-            self.df = pd.concat(
-                [self.df, pd.DataFrame({"step": [step], "reward": [average_reward_per_second]})],
-                ignore_index=True
-            )
-            self.df.to_csv(os.path.join(self.log_folder, f"rewards_{self.date_now}.csv"), index=False)
+        print(f"Step {step}, time [s] {step * self.env_dt:.2f}, "
+                f"time [min] {step * self.env_dt / 60:.2f}, "
+                f"moving average reward {average_reward_per_second:.4f}")
+        self.df = pd.concat(
+            [self.df, pd.DataFrame({"step": [step], "reward": [average_reward_per_second]})],
+            ignore_index=True
+        )
+        self.df.to_csv(os.path.join(self.log_folder, f"eval_{self.env_id}_rewards_{self.date_now}.csv"), index=False)
