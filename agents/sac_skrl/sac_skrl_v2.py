b97d4e7b65f64584e638006f1f028d5e1d654333
Sorina <lupusorina@yahoo.com>
2025-09-11 15:21:48 -0700

    cleanup skrl file

--- agents/sac_skrl/sac_skrl_v2.py
+++ agents/sac_skrl/sac_skrl_v2.py
@@ -2,7 +2,6 @@ import os
 import sys
 import json
 from datetime import datetime
-from collections import deque
 
 import torch
 import torch.nn as nn
@@ -16,11 +15,15 @@ from skrl.resources.preprocessors.torch import RunningStandardScaler
 from skrl.trainers.torch import SequentialTrainer
 from skrl.utils import set_seed
 
+import gymnasium as gym
+
 # Path setup
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../sim')))
 from ant_mujoco import AntEnv
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../embodied_ant_env')))
 from embodied_ant_env import make_ant_env
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+from reward import RewardTracker
 
 # Set seed for reproducibility
 set_seed(42)
@@ -28,7 +31,7 @@ set_seed(42)
 # define models (stochastic and deterministic models) using mixins
 class StochasticActor(GaussianMixin, Model):
     def __init__(self, observation_space, action_space, device, clip_actions=False,
-                 clip_log_std=True, min_log_std=-5, max_log_std=2):
+                 clip_log_std=True, min_log_std=-20, max_log_std=2):
         Model.__init__(self, observation_space, action_space, device)
         GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std)
 
@@ -57,8 +60,42 @@ class Critic(DeterministicMixin, Model):
     def compute(self, inputs, role):
         return self.net(torch.cat([inputs["states"], inputs["taken_actions"]], dim=1)), {}
 
+def run(agent, env):
+    obs, info = env.reset()
+    i = 0
+    action_list = []
+    while i < total_timesteps:
+        agent.pre_interaction(i, -1)
+        with torch.no_grad():
+            action = agent.act(obs, i, -1)[0]
+            action_list.append(action.cpu().numpy().flatten())
+            next_obs, reward, terminated, truncated, info = env.step(action)
+            # env.render()
+            agent.record_transition(states=obs,
+                                    actions=action,
+                                    rewards=reward,
+                                    next_states=next_obs,
+                                    terminated=terminated,
+                                    truncated=truncated,
+                                    infos=info,
+                                    timestep=i,
+                                    timesteps=total_timesteps)
 
-# Env setup
+        agent.post_interaction(timestep=i, timesteps=total_timesteps)
+        i += 1
+
+        if terminated or truncated:
+            with torch.no_grad():
+                obs, info = env.reset()
+        else:
+            obs = next_obs
+
+        average_reward_per_second = reward_tracker.update(reward.item())
+        if i % 1000 == 0:
+            reward_tracker.log(i, average_reward_per_second)
+
+
+# Env setup.
 render = "human"
 DT = 0.05
 hw_config = sys.argv[1] if len(sys.argv) > 1 else None
@@ -66,13 +103,12 @@ hw_config = sys.argv[1] if len(sys.argv) > 1 else None
 if hw_config is None:
     env_id = 'ant_mujoco'
     # env = AntEnv(
-    #     render_mode=render,
+    #     # render_mode=render,
     #     dt=DT,
     #     forward_reward_weight=1.0,
     #     ctrl_cost_weight=0.0,
     #     reward_upside_down_weight=0.0
     # )
-    import gymnasium as gym
     env = gym.make('Ant-v5')
 
 else:
@@ -81,15 +117,16 @@ else:
         cfg = json.load(f)
     env = make_ant_env(cfg, render_mode=render, dt=DT)
 
-# Wrap and prepare
+# Wrap and prepare.
 env = wrap_env(env)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Logging
+# Logging.
 LOG_FOLDER = 'logs_sac_skrl'
+DATE_NOW = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 os.makedirs(LOG_FOLDER, exist_ok=True)
 
-# Models
+# Models.
 models = {}
 models["policy"] = StochasticActor(env.observation_space, env.action_space, device, clip_actions=True)
 models["critic_1"] = Critic(env.observation_space, env.action_space, device)
@@ -97,13 +134,13 @@ models["critic_2"] = Critic(env.observation_space, env.action_space, device)
 models["target_critic_1"] = Critic(env.observation_space, env.action_space, device)
 models["target_critic_2"] = Critic(env.observation_space, env.action_space, device)
 
-# Memory
-memory = RandomMemory(memory_size=15625, device=device)
+# Memory.
+memory = RandomMemory(memory_size=1_000_000, device=device)
 
-# Config
+# Config.
 cfg = SAC_DEFAULT_CONFIG.copy()
 cfg["gradient_steps"] = 1
-cfg["batch_size"] = 256
+cfg["batch_size"] = 64
 cfg["discount_factor"] = 0.99
 cfg["polyak"] = 0.005
 cfg["actor_learning_rate"] = 5e-4
@@ -112,13 +149,16 @@ cfg["random_timesteps"] = 80
 cfg["learning_starts"] = 80
 cfg["grad_norm_clip"] = 0
 cfg["learn_entropy"] = True
-cfg["entropy_learning_rate"] = 5e-3
+cfg["entropy_learning_rate"] = 1e-3
 cfg["initial_entropy_value"] = 1.0
 cfg["state_preprocessor"] = RunningStandardScaler
 cfg["state_preprocessor_kwargs"] = {"size": env.observation_space, "device": device}
+cfg["experiment"]["write_interval"] = 800
 cfg["experiment"]["directory"] = LOG_FOLDER
+cfg["experiment"]["experiment_name"] = f"sac_skrl_{env_id}_{DATE_NOW}"
+cfg["experiment"]["checkpoint_interval"] = int(1 * 60 / DT)
 
-# Agent
+# Agent.
 agent = SAC(models=models,
             memory=memory,
             cfg=cfg,
@@ -126,43 +166,27 @@ agent = SAC(models=models,
             action_space=env.action_space,
             device=device)
 
-# Train or Eval
+reward_tracker = RewardTracker(env_dt=env.dt, env_id=env_id,
+                               log_folder=os.path.join(cfg["experiment"]["directory"], cfg["experiment"]["experiment_name"]))
+
 train = True
+train_step_by_step = True
+total_timesteps = 1_000_000
 
 if train:
     print("Training...")
+    if train_step_by_step:
+        print("Training step by step...")
+        agent.init()
+        run(agent, env)
 
-    time_in_hours = 2
-    total_timesteps = int(time_in_hours * 3600 / DT)
-    total_timesteps = 160000
-    cfg["experiment"]["checkpoint_interval"] = int(30 * 60 / DT)
-
-    trainer = SequentialTrainer(cfg={"timesteps": total_timesteps, "headless": True}, env=env, agents=agent)
-    trainer.train()
+    if train_step_by_step == False:
+        trainer = SequentialTrainer(cfg={"timesteps": total_timesteps, "headless": True}, env=env, agents=agent)
+        trainer.train()
 
 else:
-    print("Starting evaluation...")
-
-    # Find latest checkpoint
-    all_folders = sorted(
-        [f for f in os.listdir(LOG_FOLDER) if os.path.isdir(os.path.join(LOG_FOLDER, f))],
-        key=lambda x: os.path.getctime(os.path.join(LOG_FOLDER, x)),
-        reverse=True
-    )
-
-    latest_folder = None
-    for folder in all_folders:
-        checkpoint_path = os.path.join(LOG_FOLDER, folder, 'checkpoints')
-        if os.path.exists(checkpoint_path) and os.listdir(checkpoint_path):
-            latest_folder = folder
-            break
-
-    if latest_folder is None:
-        print("No valid checkpoint folder found.")
-        exit()
-
-    print(f"Using checkpoint from: {latest_folder}")
-    agent.load(os.path.join(LOG_FOLDER, latest_folder, "checkpoints", "best_agent.pt"))
-
-    trainer = SequentialTrainer(cfg={"timesteps": 1000, "headless": False}, env=env, agents=agent)
-    trainer.eval()
+    print("Evaluating...")
+    folder_path = ''
+    agent.load(folder_path)
+    agent.set_mode("eval")
+    run(agent, env)
