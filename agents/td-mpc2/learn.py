c7e2f51e86a20f55c1f985a095b287537fa56af1
Sorina Lupu <lupusorina@yahoo.com>
2025-10-14 15:38:07 -0700

    td mpc2 integration with the MuJoCo ant in sim

--- agents/td-mpc2/learn.py
+++ agents/td-mpc2/learn.py
@@ -1,11 +1,39 @@
+# MIT License
+
+# Copyright (c) Nicklas Hansen (2023).
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Modified by Sorina Lupu (sorina.lupu@openmindresearch.org)
+
 import os
-os.environ['MUJOCO_GL'] = os.getenv("MUJOCO_GL", 'egl')
+# os.environ['MUJOCO_GL'] = os.getenv("MUJOCO_GL", 'egl')
 os.environ['LAZY_LEGACY_OP'] = '0'
 os.environ['TORCHDYNAMO_INLINE_INBUILT_NN_MODULES'] = "1"
 os.environ['TORCH_LOGS'] = "+recompiles"
 import warnings
 warnings.filterwarnings('ignore')
 import torch
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../sim')))
+from ant_mujoco import AntEnv
+import numpy as np
 
 import hydra
 from termcolor import colored
@@ -15,12 +43,13 @@ sys.path.append('tdmpc2/tdmpc2')
 from common.parser import parse_cfg
 from common.seed import set_seed
 from common.buffer import Buffer
-from envs import make_env
+from envs.mujoco import make_env
 from tdmpc2 import TDMPC2
 from trainer.offline_trainer import OfflineTrainer
 from trainer.online_trainer import OnlineTrainer
 from common.logger import Logger
-
+from envs.wrappers.timeout import Timeout
+from envs.mujoco import MuJoCoWrapper
 torch.backends.cudnn.benchmark = True
 torch.set_float32_matmul_precision('high')
 
@@ -28,10 +57,10 @@ torch.set_float32_matmul_precision('high')
 @hydra.main(config_name='config', config_path='.')
 def train(cfg: dict):
 	"""
-	Script for training single-task / multi-task TD-MPC2 agents.
+	Script for training TD-MPC2 agents.
 
 	Most relevant args:
-		`task`: task name (or mt30/mt80 for multi-task training)
+		`task`: task name
 		`model_size`: model size, must be one of `[1, 5, 19, 48, 317]` (default: 5)
 		`steps`: number of training/environment steps (default: 10M)
 		`seed`: random seed (default: 1)
@@ -51,16 +80,36 @@ def train(cfg: dict):
 	set_seed(cfg.seed)
 	print(colored('Work dir:', 'yellow', attrs=['bold']), cfg.work_dir)
 
+	if cfg.task == 'embodied-ant':
+		DT = 0.05
+		joint_config = {
+			'hip_zero': 0,
+			'knee_zero': -np.radians(50),
+			'hip_range': np.radians(30),
+			'knee_range': np.radians(20),
+		}
+		env = AntEnv(
+				dt=DT,
+				render_mode="human",
+				joint_config=joint_config,
+		)
+	else:
+		raise ValueError('Unknown task:', cfg.task)
+	env = MuJoCoWrapper(env, cfg)
+	env = Timeout(env, max_episode_steps=1000)
+	cfg.discount_max = 0.99
+	cfg.rho = 0.7
+
 	trainer_cls = OnlineTrainer
 	trainer = trainer_cls(
 		cfg=cfg,
-		env=make_env(cfg),
+		env=env,
 		agent=TDMPC2(cfg),
 		buffer=Buffer(cfg),
 		logger=Logger(cfg),
 	)
 	trainer.train()
-	print('\nTraining completed successfully')
+	print('\nLearning completed successfully')
 
 
 if __name__ == '__main__':
