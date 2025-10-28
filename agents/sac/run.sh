1c49f720acd4036d984209b23a19cd61a46a0c4b
Sorina Lupu <lupusorina@yahoo.com>
2025-10-27 22:07:32 -0700

    runs hw and sim

--- agents/sac/run.sh
+++ agents/sac/run.sh
@@ -2,13 +2,8 @@
 
 # learn in sim
 if [ "$1" == "sim" ]; then
-    python3 sac_cleanrl.py --render_mode rgb_array --capture_video --dt 0.12 --env_id SimEmbodiedAnt --learning_starts 2000
+    python3 sac_cleanrl.py --render_mode rgb_array --dt 0.12 --env_id SimEmbodiedAnt --learning_starts 2000
 fi
-
-# # learn in hardware
-# if [ "$1" == "hw" ]; then
-#     python3 sac_cleanrl.py --render_mode rgb_array --capture_video --dt 0.1 --env_id HwEmbodiedAnt --hw_config /Users/sorinalupu/OpenmindResearch/workshops/EmbodiedAnt/embodied_ant_env/ant34.json --weights_path /Users/sorinalupu/OpenmindResearch/workshops/EmbodiedAnt/agents/sac_skrl/runs/SimEmbodiedAnt__sac_cleanrl__1__20251011-124337/weights --learning_starts 20
-# fi
 
 if [ "$1" == "hw" ]; then
     python3 sac_cleanrl.py --render_mode rgb_array --dt 0.12 --env_id HwEmbodiedAnt --hw_config ../../embodied_ant_env/ant12.json --learning_starts 2000
