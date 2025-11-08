fb0816264eacf43e3ce5b5e365f2bb0dcc5f8326
Sorina <lupusorina@yahoo.com>
2025-11-08 12:28:42 -0800

    remove seaborn from reward

--- agents/reward.py
+++ agents/reward.py
@@ -2,7 +2,6 @@ import os
 import pandas as pd
 from collections import deque
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 
 class RewardTracker:
@@ -46,7 +45,6 @@ class RewardTracker:
 
     def plot(self, save_path=None):
         plt.figure(figsize=(10, 5))
-        sns.set_theme(style="whitegrid")
         plt.plot(
             self.df["step"][self.window_size:] * self.env_dt,
             self.df["reward"][self.window_size:],
