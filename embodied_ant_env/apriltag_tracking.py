2cd62504cebb7abd4f0cd8c900ba86134bd6fd29
Sorina Lupu <lupusorina@yahoo.com>
2025-11-13 18:03:44 -0800

    feat(apriltag-tracking): add task-circle visualization and improve plotting

--- embodied_ant_env/apriltag_tracking.py
+++ embodied_ant_env/apriltag_tracking.py
@@ -192,9 +192,17 @@ if __name__ == "__main__":
             print(f"{tag_id}: \n{detection['orientation']}")
 
         if 'body' in bodies:
-            plt.plot(bodies['body']['position'][0], bodies['body']['position'][1], 'o')
-        plt.plot(0, 0, 'o', color='red')
+            plt.plot(bodies['body']['position'][0], bodies['body']['position'][1], 'o', color='blue')
+
+        # Plot a circle with the radius of the task
+        radius = 0.61
+        origin = np.array([0.14194049, -0.82257924])
+        plt.plot(origin[0], origin[1], 'o', color='red')
+        plt.plot(origin[0] + radius*np.cos(np.linspace(0, 2*np.pi, 100)), origin[1] + radius*np.sin(np.linspace(0, 2*np.pi, 100)), color='red')
+        plt.plot(0, 0, 'o', color='black')
         plt.pause(0.01)
+        # equal aspect ratio
+        plt.gca().set_aspect('equal', adjustable='box')
         plt.show(block=False)
         plt.grid(True)
         # plt.clf()
