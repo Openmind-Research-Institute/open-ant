828428373c75c266aa15d45955401c43073ff016
Sorina Lupu <lupusorina@yahoo.com>
2025-11-25 15:10:17 -0800

    run sim multiple seeds

--- agents/sac/run.sh
+++ agents/sac/run.sh
@@ -7,7 +7,82 @@ if [ "$1" == "sim" ]; then
         --dt 0.12 \
         --env_id SimEmbodiedAnt \
         --learning_starts 2000 \
-        --capture_video true
+        --task_type back_and_forth \
+        --seed 1 \
+        --total_timesteps 35000
+
+    python3 sac_cleanrl.py \
+        --render_mode rgb_array \
+        --dt 0.12 \
+        --env_id SimEmbodiedAnt \
+        --learning_starts 2000 \
+        --task_type back_and_forth \
+        --seed 2
+
+    python3 sac_cleanrl.py \
+        --render_mode rgb_array \
+        --dt 0.12 \
+        --env_id SimEmbodiedAnt \
+        --learning_starts 2000 \
+        --task_type back_and_forth \
+        --seed 3
+    
+    python3 sac_cleanrl.py \
+        --render_mode rgb_array \
+        --dt 0.12 \
+        --env_id SimEmbodiedAnt \
+        --learning_starts 2000 \
+        --task_type back_and_forth \
+        --seed 4
+
+    python3 sac_cleanrl.py \
+        --render_mode rgb_array \
+        --dt 0.12 \
+        --env_id SimEmbodiedAnt \
+        --learning_starts 2000 \
+        --task_type back_and_forth \
+        --seed 5
+
+    python3 sac_cleanrl.py \
+        --render_mode rgb_array \
+        --dt 0.12 \
+        --env_id SimEmbodiedAnt \
+        --learning_starts 2000 \
+        --task_type back_and_forth \
+        --seed 6
+
+    python3 sac_cleanrl.py \
+        --render_mode rgb_array \
+        --dt 0.12 \
+        --env_id SimEmbodiedAnt \
+        --learning_starts 2000 \
+        --task_type back_and_forth \
+        --seed 7
+
+    python3 sac_cleanrl.py \
+        --render_mode rgb_array \
+        --dt 0.12 \
+        --env_id SimEmbodiedAnt \
+        --learning_starts 2000 \
+        --task_type back_and_forth \
+        --seed 8
+
+    python3 sac_cleanrl.py \
+        --render_mode rgb_array \
+        --dt 0.12 \
+        --env_id SimEmbodiedAnt \
+        --learning_starts 2000 \
+        --task_type back_and_forth \
+        --seed 9
+
+    python3 sac_cleanrl.py \
+        --render_mode rgb_array \
+        --dt 0.12 \
+        --env_id SimEmbodiedAnt \
+        --learning_starts 2000 \
+        --task_type back_and_forth \
+        --seed 10
+
 fi
 
 # learn in sim with Optuna optimization
@@ -24,11 +99,13 @@ fi
 
 # learn on hardware (single run, no Optuna)
 if [ "$1" == "hw" ]; then
-    python3 sac_cleanrl_optuna.py \
+    python3 sac_cleanrl.py \
         --render_mode rgb_array \
         --dt 0.12 \
         --env_id HwEmbodiedAnt \
         --hw_config ../../embodied_ant_env/ant12.json \
-        --learning_starts 2000
+        --learning_starts 2000 \
+        --task_type forward \
+        --render_mode human
 fi
 
