dd04a7262388d9af8f64b850631422604b97e635
Sorina Lupu <lupusorina@yahoo.com>
2026-04-13 16:33:16 -0700

    update hw setting

--- agents/sac/run.sh
+++ agents/sac/run.sh
@@ -16,12 +16,11 @@ if [ "$1" == "hw" ]; then
         --render_mode rgb_array \
         --dt 0.12 \
         --env_id HwEmbodiedAnt \
-        --hw_config ../../embodied_ant_env/ant34.json \
+        --hw_config ../../embodied_ant_env/ant12.json \
         --learning_starts 2000 \
         --task_type back_and_forth \
-        --runs_directory runs_hw \
+        --runs_directory runs_hw_new_refactored_code \
         --exp_name trial_1 \
-        --use_layer_norm \
         --seed 1 \
         # --eval True
 fi
