46c27ad354d0f609355a4b43571add021648b3c0
mdecke <mdecke@student.ethz.ch>
2026-04-23 10:48:04 -0700

    run mpo experiments

---
+++ agents/mpo/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Learn in simulation.
+if [ "$1" == "sim" ]; then
+    python3 mpo_pytorch.py \
+        --render_mode rgb_array \
+        --env_id SimEmbodiedAnt \
+        --runs_directory runs_sim_test \
+        --exp_name trial_1
+fi
+
+# Learn on hardware.
+if [ "$1" == "hw" ]; then
+    python3 mpo_pytorch.py \
+        --render_mode rgb_array \
+        --dt 0.12 \
+        --env_id HwEmbodiedAnt \
+        --hw_config ../../embodied_ant_env/ant34.json \
+        --learning_starts 2000 \
+        --task_type back_and_forth \
+        --runs_directory runs_hw \
+        --exp_name trial_1 \
+        --use_layer_norm \
+        --seed 1 \
+        # --eval True
+fi
