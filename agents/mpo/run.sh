6eb1e20e3b786418f3e194c59647173439083cc0
mdecke <mdecke@student.ethz.ch>
2026-04-28 16:35:25 -0700

    update run params for hw

--- agents/mpo/run.sh
+++ agents/mpo/run.sh
@@ -12,12 +12,12 @@ fi
 
 # Learn on hardware.
 if [ "$1" == "hw" ]; then
-    python3 mpo_pytorch.py \
+    python3 mpo_default.py \
         --render_mode rgb_array \
         --dt 0.15 \
         --env_id HwEmbodiedAnt \
         --hw_config ../../embodied_ant_env/ant12.json \
-        --learning_starts 100 \
+        --learning_starts 500 \
         --task_type back_and_forth \
         --runs_directory runs_hw \
         --exp_name trial_1 \
