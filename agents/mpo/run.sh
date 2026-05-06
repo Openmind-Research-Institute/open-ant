709028990aa5cdd04a59ca2cb8d7a73dbcbb7ea5
mdecke <mdecke@student.ethz.ch>
2026-05-06 10:29:26 -0700

    updated experiment run file

--- agents/mpo/run.sh
+++ agents/mpo/run.sh
@@ -7,11 +7,11 @@ if [ "$1" == "sim" ]; then
         --dt 0.15 \
         --env_id SimEmbodiedAnt \
         --runs_directory runs \
-        --exp_name decoupled \
+        --exp_name og_kl \
         --utd_ratio 3 \
         --ensemble 3 \
         --decouple_q_learning \
-        --policy_learning_starts 5000 \
+        --policy_learning_starts 1000 \
         --td_horizon 3
 fi
 
@@ -22,10 +22,15 @@ if [ "$1" == "hw" ]; then
         --dt 0.15 \
         --env_id HwEmbodiedAnt \
         --hw_config ../../embodied_ant_env/ant12.json \
-        --learning_starts 500 \
+        --learning_starts 2000 \
         --task_type back_and_forth \
         --runs_directory runs_hw \
-        --exp_name trial_1 \
+        --exp_name trial_3 \
         --seed 1 \
+        --utd_ratio 3 \
+        --ensemble 3 \
+        --decouple_q_learning \
+        --policy_learning_starts 1000 \
+        --td_horizon 3
         # --eval True
 fi
