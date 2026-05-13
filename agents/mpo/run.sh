55483613deaa772b3074cd0cada2a22de7b55b67
mdecke <mdecke@student.ethz.ch>
2026-05-13 14:30:12 -0700

    new run hyper params

--- agents/mpo/run.sh
+++ agents/mpo/run.sh
@@ -14,14 +14,15 @@ if [ "$1" == "sim" ]; then
             --dt 0.15 \
             --env_id SimEmbodiedAnt \
             --runs_directory runs \
-            --exp_name "seed_${SEED}_trial" \
+            --exp_name test \
             --utd_ratio 3 \
             --ensemble 3 \
             --decouple_q_learning \
             --policy_learning_starts 1000 \
             --td_horizon 3 \
-            --seed $SEED
-            # --weights_path /Users/mathieudecker/embodied-mujoco-ant/agents/mpo/runs/new_baseline_20260506-085105_seed_1/weights_and_args
+            --seed $SEED \
+            # --weights_path /Users/mathieudecker/embodied-mujoco-ant/agents/mpo/runs/seed_459_trial_helios_20260507-180606_seed_459/weights_and_args \
+            # --eval
         echo "trial ${SEED} done"
     done
 fi
@@ -30,19 +31,20 @@ fi
 if [ "$1" == "hw" ]; then
     python3 mpo_default.py \
         --render_mode rgb_array \
-        --dt 0.15 \
+        --dt 0.20 \
+        --total_timesteps 60000 \
         --env_id HwEmbodiedAnt \
         --hw_config ../../embodied_ant_env/ant12.json \
         --learning_starts 2000 \
         --task_type back_and_forth \
         --runs_directory runs_hw \
-        --exp_name sim2real_cntd \
+        --exp_name larger_dt \
         --seed 1 \
         --utd_ratio 3 \
         --ensemble 3 \
         --decouple_q_learning \
         --policy_learning_starts 1000 \
         --td_horizon 3 \
-        --weights_path /Users/mathieudecker/embodied-mujoco-ant/agents/mpo/runs_hw/sim2real_20260506-153614_seed_1/weights_and_args
-        # --eval True
+        # --weights_path /Users/mathieudecker/embodied-mujoco-ant/agents/mpo/runs/seed_459_trial_helios_20260507-180606_seed_459/weights_and_args \
+        # --eval
 fi
