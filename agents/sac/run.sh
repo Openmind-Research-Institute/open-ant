cd09bf11a000afd732c0ed1163f9e9080dd391b9
Sorina <lupusorina@yahoo.com>
2026-05-18 22:21:34 -0700

    add the two different runs

--- agents/sac/run.sh
+++ agents/sac/run.sh
@@ -5,10 +5,27 @@ if [ "$1" == "sim" ]; then
     python3 sac_cleanrl.py \
         --render_mode rgb_array \
         --env_id SimEmbodiedAnt \
-        --runs_directory runs_sim_test \
+        --runs_directory runs_sim_less_aggresive \
         --exp_name trial_1 \
-        --num_envs 1
+        --num_envs 1 \
+        --radius_back_and_forth 1.0 \
+        --origin_back_and_forth 0.0 0.0
 fi
+
+if [ "$1" == "sim_continual_learning" ]; then
+    python3 sac_cleanrl.py \
+        --render_mode rgb_array \
+        --env_id SimEmbodiedAnt \
+        --runs_directory runs_sim_less_aggresive \
+        --exp_name trial_1_continual_learning \
+        --num_envs 1 \
+        --weights_path /home/sorina/linc/open-ant/agents/sac/runs_sim_less_aggresive/trial_1_20260518-220724_seed_1 \
+        --total_timesteps 120_000 \
+        --model_path ../../sim/assets/ant_with_camera_after_sys_id_real_less_aggresive.xml \
+        --radius_back_and_forth 1.0 \
+        --origin_back_and_forth 0.0 0.0
+fi
+
 
 # Learn on hardware.
 if [ "$1" == "hw" ]; then
