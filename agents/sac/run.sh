f8443641f0853368b4de82f07ff725d04c696be3
Serena Liu <zarinaliu@gmail.com>
2026-05-29 19:19:30 -0400

    implementing mixing offline and online buffers for sim2 training

--- agents/sac/run.sh
+++ agents/sac/run.sh
@@ -9,7 +9,8 @@ if [ "$1" == "sim" ]; then
         --exp_name trial_1 \
         --num_envs 1 \
         --radius_back_and_forth 1.0 \
-        --origin_back_and_forth 0.0 0.0
+        --origin_back_and_forth 0.0 0.0 \
+        --cuda &
 fi
 
 if [ "$1" == "sim_continual_learning" ]; then
@@ -19,11 +20,13 @@ if [ "$1" == "sim_continual_learning" ]; then
         --runs_directory runs_sim_less_aggresive \
         --exp_name trial_1_continual_learning \
         --num_envs 1 \
-        --weights_path /home/sorina/linc/open-ant/agents/sac/runs_sim_less_aggresive/trial_1_20260518-220724_seed_1 \
+        --weights_path /home/serena-liu/open-ant/agents/sac/runs_sim_less_aggresive/trial_1_20260529-011538_seed_42 \
+        --offline_buffer_path /home/serena-liu/open-ant/agents/sac/runs_sim_less_aggresive/trial_1_20260529-011538_seed_42/replay_buffer \
         --total_timesteps 120_000 \
         --model_path ../../sim/assets/ant_with_camera_after_sys_id_real_less_aggresive.xml \
         --radius_back_and_forth 1.0 \
-        --origin_back_and_forth 0.0 0.0
+        --origin_back_and_forth 0.0 0.0 \
+        --cuda &
 fi
 
 
