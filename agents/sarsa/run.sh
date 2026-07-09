b403de2f359aa82211788cde04f24c12d1719e0a
Sorina <lupusorina@yahoo.com>
2026-07-09 07:06:42 -0700

    run for sarsa with the 30 seeds

--- agents/sarsa/run.sh
+++ agents/sarsa/run.sh
@@ -5,10 +5,63 @@ if [ "$1" == "sim" ]; then
         --render_mode rgb_array \
         --dt 0.05 \
         --env_id SimEmbodiedAnt \
-        --runs_directory runs_sarsa_sim \
+        --runs_directory runs_sarsa_sim_evaluate_optuna \
         --exp_name trial_1 \
-        --capture_video
+        --epsilon 0.26 \
+        --discount 0.98 \
+        --lambda_eligibility 0.94 \
+        --dim_tiling 4 \
+        --tilings_multiplier 8 \
+        --step_size_base 0.001 \
+        --reward_scaling 5 \
+        --seed 0
 fi
+
+
+if [ "$1" == "sim_30_seeds" ]; then
+    for seed in {0..29}; do
+        python3 sarsa_options_tilecoding.py \
+            --render_mode rgb_array \
+            --dt 0.05 \
+            --env_id SimEmbodiedAnt \
+            --runs_directory runs_sarsa_sim_30_seeds \
+            --exp_name trial_$seed \
+            --epsilon 0.26 \
+            --discount 0.98 \
+            --lambda_eligibility 0.94 \
+            --dim_tiling 4 \
+            --tilings_multiplier 8 \
+            --step_size_base 0.001 \
+            --reward_scaling 5 \
+            --seed "$seed" &
+    done
+
+    wait
+fi
+
+
+
+if [ "$1" == "sim_30_seeds_v2" ]; then
+    for seed in {0..29}; do
+        python3 sarsa_options_tilecoding.py \
+            --render_mode rgb_array \
+            --dt 0.05 \
+            --env_id SimEmbodiedAnt \
+            --runs_directory runs_sarsa_sim_30_seeds_v2 \
+            --exp_name trial_$seed \
+            --epsilon 0.255 \
+            --discount 0.998 \
+            --lambda_eligibility 0.964\
+            --dim_tiling 4 \
+            --tilings_multiplier 8 \
+            --step_size_base 0.008 \
+            --reward_scaling 5 \
+            --seed "$seed" &
+    done
+
+    wait
+fi
+
 
 # Learn on hardware.
 if [ "$1" == "hw" ]; then
