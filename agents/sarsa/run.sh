25f3dacd42ec82744586cc613a694cbd6c9c8e67
mathieudecker <mathieu.a.decker@jpl.nasa.gov>
2026-07-14 12:56:02 -0700

    cleanup runs

--- agents/sarsa/run.sh
+++ agents/sarsa/run.sh
@@ -5,16 +5,9 @@ if [ "$1" == "sim" ]; then
         --render_mode rgb_array \
         --dt 0.05 \
         --env_id SimEmbodiedAnt \
-        --runs_directory runs_sarsa_sim_final_paper \
+        --runs_directory runs_sarsa_sim \
         --exp_name trial_1 \
-        --epsilon 0.255 \
-        --discount 0.998 \
-        --lambda_eligibility 0.964 \
-        --dim_tiling 4 \
-        --tilings_multiplier 8 \
-        --step_size_base 0.008 \
-        --reward_scaling 5 \
-        --seed 0
+        --seed 1
 fi
 
 
@@ -28,12 +21,4 @@ if [ "$1" == "hw" ]; then
         --seed 1 \
         --runs_directory runs_sarsa_hw \
         --exp_name trial_1 \
-        --runs_directory runs_sarsa_hw_final_paper \
-        --epsilon 0.255 \
-        --discount 0.998 \
-        --lambda_eligibility 0.964 \
-        --dim_tiling 4 \
-        --tilings_multiplier 8 \
-        --step_size_base 0.008 \
-        --reward_scaling 5
 fi
